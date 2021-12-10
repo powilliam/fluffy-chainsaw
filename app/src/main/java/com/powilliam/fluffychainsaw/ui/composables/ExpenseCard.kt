8918906f3f4135a7c87797ea3c5507bec9d2517c
William Porto <william.porto@nave.rs>
2021-12-10 02:56:33 -0300

    refactor(composables): new expense card design but with expense category mocked

--- app/src/main/java/com/powilliam/fluffychainsaw/ui/composables/ExpenseCard.kt
+++ app/src/main/java/com/powilliam/fluffychainsaw/ui/composables/ExpenseCard.kt
@@ -1,22 +1,26 @@
 package com.powilliam.fluffychainsaw.ui.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.powilliam.fluffychainsaw.data.entities.Expense
 import com.powilliam.fluffychainsaw.data.entities.ExpenseType
 import com.powilliam.fluffychainsaw.ui.theme.FluffyChainsawTheme
 import java.text.NumberFormat
-import com.powilliam.fluffychainsaw.R
 
 @Composable
 fun ExpenseCard(
@@ -31,34 +35,46 @@ fun ExpenseCard(
                 .clickable(onClick = onClick)
                 .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            val chipColor = when (expense.type) {
-                ExpenseType.Variable -> MaterialTheme.colorScheme.tertiaryContainer
-                else -> MaterialTheme.colorScheme.secondaryContainer
-            }
-
-            Column(modifier.fillMaxWidth(fraction = 0.65F)) {
-                val cost = try {
-                    NumberFormat
-                        .getCurrencyInstance()
-                        .also { formatter ->
-                            formatter.maximumFractionDigits = 2
-                        }
-                        .format(expense.cost)
-                } catch (exception: NumberFormatException) {
-                    expense.cost
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.tertiaryContainer)
+                ) {
+                    Icon(
+                        modifier = modifier.align(Alignment.Center),
+                        imageVector = Icons.Rounded.SportsEsports,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
                 }
+                Spacer(modifier.width(16.dp))
+                Column {
+                    val cost = try {
+                        NumberFormat
+                            .getCurrencyInstance()
+                            .also { formatter ->
+                                formatter.maximumFractionDigits = 2
+                            }
+                            .format(expense.cost)
+                    } catch (exception: NumberFormatException) {
+                        expense.cost
+                    }
 
-                Text(text = expense.name, style = MaterialTheme.typography.headlineSmall)
-                Text(text = "$cost", style = MaterialTheme.typography.bodyLarge)
-            }
-            Chip(color = chipColor) {
-                val id = when (expense.type) {
-                    ExpenseType.Fixed -> R.string.expense_type_fixed
-                    else -> R.string.expense_type_variable
+                    Text(text = expense.name, style = MaterialTheme.typography.headlineSmall)
+                    Text(text = "$cost", style = MaterialTheme.typography.bodyMedium)
                 }
-
-                Text(text = stringResource(id))
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Spacer(modifier.width(16.dp))
+                Icon(
+                    imageVector = Icons.Rounded.DragHandle,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.outline
+                )
             }
         }
     }
